92cf5d6b63b46a15b26e6e7726e3111dd4f53191
Omodamola <darmhoo@hotmail.com>
2022-09-11 05:59:58 +0100

    Update src/utilities/fetchListFromApi.ts
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/utilities/fetchListFromApi.ts
+++ src/utilities/fetchListFromApi.ts
@@ -5,7 +5,7 @@ const standUrl = "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84";
 async function fetchUsers(page: any = null, url: any = standUrl) {
   console.log("igothre");
     try{
-        let list = await axios.get(url + (page ? `&page=${page}` : ''));
+    let list = await axios.get(url + (page ? `&page=${page}` : ""));
        return list.data;
   } catch (err) {
         console.log(err)
